613a28c870d3074f7696bab03e8600961cd7fe87
Hyperdrax <florinator543@gmail.com>
2023-12-15 17:47:32 +0100

    fixed

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: fhensel <fhensel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 11:20:04 by fhensel           #+#    #+#             */
-/*   Updated: 2023/12/15 17:46:08 by fhensel          ###   ########.fr       */
+/*   Updated: 2023/12/15 17:47:26 by fhensel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_strncmp(const char *s, const char *w, size_t n)
 	i = 0;
 	if (n != 0)
 	{
-		while (i < n - 1 && s[i] != '\0' && w[i] != '\0' && w[i] == w[i])
+		while (i < n - 1 && s[i] != '\0' && w[i] != '\0' && s[i] == w[i])
 			i++;
 		return ((unsigned char)s[i] - (unsigned char)w[i]);
 	}

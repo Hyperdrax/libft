6952233eec224acf9a7c82dbb920d3986934b64c
Hyperdrax <florinator543@gmail.com>
2023-12-18 17:40:17 +0100

    fixed

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: fhensel <fhensel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 08:11:17 by fhensel           #+#    #+#             */
-/*   Updated: 2023/12/18 17:16:31 by fhensel          ###   ########.fr       */
+/*   Updated: 2023/12/18 17:35:18 by fhensel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int		i;
+	int	i;
 
-	i = ft_strlen(s);
-	if (c == 0)
-		return ((char *)s + i);
+	i = 0;
+	while (s[i])
+		i++;
 	while (i >= 0)
 	{
-		if (s[i] == c)
-			return ((char *)s + i);
+		if (s[i] == (char)c)
+			return ((char *)(s + i));
 		i--;
 	}
 	return (NULL);

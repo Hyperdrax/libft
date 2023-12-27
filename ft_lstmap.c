b898eadd76e8f1389c1320e9a749d61e1a0468cb
Hyperdrax <florinator543@gmail.com>
2023-12-27 11:01:38 +0100

    minor change

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: fhensel <fhensel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/26 12:41:35 by fhensel           #+#    #+#             */
-/*   Updated: 2023/12/27 10:27:31 by fhensel          ###   ########.fr       */
+/*   Updated: 2023/12/27 10:56:46 by fhensel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*f_list;
 
-	if (!lst || !f)
+	if (!lst || !f || !del)
 		return (NULL);
 	f_list = ft_lstnew(f(lst->content));
 	if (!f_list)
